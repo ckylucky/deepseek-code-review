71c798287d9994fe8932082b52331cc54cdcbcd0
lucky <1094568708@qq.com>
2025-03-25 11:55:49 +0800

    init

---
+++ deepseek-code-review-sdk/src/main/java/com/cky/sdk/DeepseekCodeReview.java
@@ -0,0 +1,14 @@
+package com.cky.sdk;
+
+/**
+ * @ClassName ${NAME}
+ * @Description
+ * @Author lukcy
+ * @Date 2025/3/25 11:42
+ * @Version 1.0
+ */
+public class DeepseekCodeReview {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
